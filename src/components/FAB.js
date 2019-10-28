cdbe7082facd9ef17bef7610637e94acc43e047c
Quốc Khánh <bkdev98@gmail.com>
2019-10-28 20:23:32 +0700

    touchable scale

--- src/components/FAB.js
+++ src/components/FAB.js
@@ -1,36 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { LinearGradient } from 'expo-linear-gradient';
-import { TouchableOpacity } from 'react-native';
+import { TouchableWithoutFeedback, Animated } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
-const FAB = ({ icon, onPress }) => (
-  <TouchableOpacity
-    style={{
-      width: 60,
-      height: 60,
-      borderRadius: 30,
-      position: 'absolute',
-      bottom: 40,
-      right: 20,
-      zIndex: 99,
-    }}
-    onPress={onPress}
-  >
-    <LinearGradient
-      colors={['#FB9ED3', '#EB7483']}
-      start={[1,0]}
-      end={[0,1]}
+const FAB = ({ icon, onPress, scaleValue }) => {
+  const [animatedValue] = useState(new Animated.Value(1));
+
+  const handlePressIn = () => {
+    Animated.spring(animatedValue, {
+      toValue: scaleValue || 0.94,
+    }).start();
+  };
+
+  const handlePressOut = () => {
+    Animated.spring(animatedValue, {
+      toValue: 1,
+      friction: 3,
+      tension: 40,
+    }).start();
+  };
+
+  const animatedStyle = {
+    transform: [
+      {
+        scale: animatedValue,
+      },
+    ],
+  };
+
+  return (
+    <TouchableWithoutFeedback
       style={{
         width: 60,
         height: 60,
         borderRadius: 30,
-        alignItems: 'center',
-        justifyContent: 'center',
       }}
+      onPressIn={handlePressIn}
+      onPressOut={handlePressOut}
+      onPress={onPress}
     >
-      <Feather name={icon} size={20} color="#FFFFFF" />
-    </LinearGradient>
-  </TouchableOpacity>
-);
+      <Animated.View style={{
+        width: 60,
+        height: 60,
+        position: 'absolute',
+        bottom: 40,
+        right: 20,
+        zIndex: 99,
+        ...animatedStyle,
+      }}>
+        <LinearGradient
+          colors={['#FB9ED3', '#EB7483']}
+          start={[1,0]}
+          end={[0,1]}
+          style={{
+            width: 60,
+            height: 60,
+            borderRadius: 30,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Feather name={icon} size={20} color="#FFFFFF" />
+        </LinearGradient>
+      </Animated.View>
+    </TouchableWithoutFeedback>
+  );
+}
 
 export default FAB;
